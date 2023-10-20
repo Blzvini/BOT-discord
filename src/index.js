aeaa6edcf1982ce3352705906062c27cb96fce3a
Vinicius Lima <vinicius.lima.brasil1@gmail.com>
2023-10-20 04:53:39 -0300

    FEAT: adicionado a funcionalidade de commandos por barra(slash commands)

--- src/index.js
+++ src/index.js
@@ -1,10 +1,33 @@
 require('dotenv').config();
-const { Client, GatewayIntentBits, EmbedBuilder, PermissionsBitField, Permissions } = require('discord.js');
-const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] })
+const { Client, GatewayIntentBits, EmbedBuilder, PermissionsBitField, Permissions, IntentsBitField } = require('discord.js');
+//const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] })
+
+const client = new Client({
+    intents: [
+        IntentsBitField.Flags.Guilds,
+        IntentsBitField.Flags.GuildMembers,
+        IntentsBitField.Flags.GuildMessages,
+        IntentsBitField.Flags.MessageContent,
+    ]
+})
+
 
 client.on("ready", (x) => {
     console.log(`${x.user.tag} is ready`);
-    client.user.setActivity(`Bot Vini Vini em ação`)
+    client.user.setActivity(`${x.user.tag} is online.`)
+})
+
+client.on('interactionCreate', (interaction) => {
+    if (!interaction.isChatInputCommand()) return;
+
+    if (interaction.commandName === 'hey') {
+        interaction.reply('Hey')
+    }
+
+    if (interaction.commandName === 'zietz') {
+        interaction.reply('Salve corninho!')
+    }
+
 })
 
 
