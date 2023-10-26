ebde5ec046455d0acf44f47cc9c7bd0393f2390f
Vinicius Lima <vinicius.lima.brasil1@gmail.com>
2023-10-25 21:12:09 -0300

    feat: funcionalidade de checar aniversarios cadastrados uma vez ao dia com a data atual e enviar uma mensagem no canal configurado

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
 require('dotenv').config();
+const cron = require('cron');
+
+
+
 const { Client, GatewayIntentBits, EmbedBuilder, PermissionsBitField, Permissions, IntentsBitField } = require('discord.js');
 //const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] })
 
@@ -11,50 +15,79 @@ const client = new Client({
     ]
 })
 
+function formatDate(date) {
+    const dateFormat = new Date(date.replace(/-/g, '\/'));
+    let newDate = ('0' + dateFormat.getDate()).slice(-2) + '/'
+        + ('0' + (dateFormat.getMonth() + 1)).slice(-2) + '/'
+        + dateFormat.getFullYear();
+    console.log(MyDateString)
+    console.log(birthdayComparable)
+    return newDate;
+}
+
 
 client.on("ready", (x) => {
+
     console.log(`${x.user.tag} is ready`);
+
     client.user.setActivity(`${x.user.tag} is online.`)
 })
 
-client.on('interactionCreate', (interaction) => {
-    if (!interaction.isChatInputCommand()) return;
+client.on('messageCreate', (message) => {
 
-    if (interaction.commandName === 'hey') {
-        interaction.reply('Hey')
-    }
-
-    if (interaction.commandName === 'zietz') {
-        interaction.reply('Salve corninho!')
-    }
-
-    if (interaction.commandName === 'fibo') {
-        const inputNumber = interaction.options.getInteger('int');
-
-        if (isNaN(inputNumber) || !Number.isInteger(inputNumber) || inputNumber < 1) {
+    /* const embed = new EmbedBuilder()
+         .setTitle('Feliz Aniversário')
+         .setDescription('Ta ficando mais velho é?')
+         .setColor('Random')
+         .setURL('https://www.youtube.com/watch?v=5DD8fhgJGfw')
+ 
+     message.reply({ embeds: [embed] });
+     return;
+     */
 
 
+});
 
-            interaction.reply('Por favor, forneça um número inteiro válido maior ou igual a 1.');
-        } else {
-            const fibonacciSequence = generateFibonacci(inputNumber);
-            interaction.reply(`Sequência de Fibonacci com os primeiros ${inputNumber} números: ${fibonacciSequence.join(', ')}`);
-        }
-    }
+const birthdayDates = [
+    new Date('2000/9/26'),
+    new Date('2003/10/25'),
+    new Date('2000/2/8'),
+    new Date('1999/2/8'),
+
+]
+
+client.once("ready", () => {
+    console.log(`Online as ${client.user.tag}`);
+    const timeElapsed = Date.now();
+    const today = new Date(timeElapsed);
+
+    const embed = new EmbedBuilder()
+        .setTitle('Feliz Aniversário')
+        .setDescription('Ta ficando mais velho é?')
+        .setColor('Random')
+        .setURL('https://www.youtube.com/watch?v=5DD8fhgJGfw')
+
+    let scheduledMessage = new cron.CronJob('* * * * *', () => {
+        // This runs every day at 10:30:00, you can do anything you want
+        // Specifing your guild (server) and your channel
+
+
+        birthdayDates.forEach((date) => {
+            if (date.getDate() == today.getDate() && date.getMonth() == today.getMonth()) {
+                const guild = client.guilds.cache.get(process.env.GUILD_ID);
+                const channel = guild.channels.cache.get(process.env.CHANNEL_ID);
+                channel.send({ embeds: [embed] });
+            }
+
+        })
+
+    });
+
+    // When you want to start it, use:
+    scheduledMessage.start()
 });
 
 
-function generateFibonacci(n) {
-    const sequence = [0, 1];
-
-
-    while (sequence.length < n) {
-        const nextValue = sequence[sequence.length - 1] + sequence[sequence.length - 2];
-        sequence.push(nextValue);
-    }
-
-    return sequence;
-}
 
 
 client.login(process.env.TOKEN)
--- src/register-commands.js
+++ src/register-commands.js
@@ -5,28 +5,20 @@ const commands = [
     {
         name: 'hey',
         description: 'Replies with hey',
+        options: [
+            {
+                name: 'option-1',
+                description: 'aaaaaaaa',
+                type: boolean
+            }
+
+        ]
     },
     {
         name: 'zietz',
         description: 'Salve pro corninho',
     },
-    {
-<<<<<<< HEAD
-        name: 'felipe',
-        description: 'gordao emprestimos',
-=======
-        name: 'fibo',
-        description: 'Retorna uma sequência fibonacci',
-        options: [
-            {
-                name: 'int',
-                description: 'Número inteiro',
-                type: 4, 
-                required: true,
-            },
-        ],
->>>>>>> 9be9f42cb96786985d6fcba1f8c526f5dc098752
-    },
+
 ];
 
 const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
